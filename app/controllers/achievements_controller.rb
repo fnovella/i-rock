c17684962444933f82769c5681cddb35497cbde8
Javier Novella <fnovella@gmail.com>
2020-04-28 16:58:22 +0200

    Controller tests done

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,4 +1,7 @@
 class AchievementsController < ApplicationController
+  def index
+    @achievements = Achievement.public_access
+  end
   def new
     @achievement = Achievement.new
   end
@@ -6,14 +9,32 @@ class AchievementsController < ApplicationController
   def create
     @achievement = Achievement.new(achievement_params)
     if @achievement.save
-      redirect_to root_url, notice: 'Achievement has been created'
+      redirect_to achievement_url(@achievement), notice: 'Achievement has been created'
     else
       render :new
     end
   end
 
+  def edit
+    @achievement = Achievement.find(params[:id])
+  end
+
+  def update
+    @achievement = Achievement.find(params[:id])
+    if @achievement.update(achievement_params)
+      redirect_to achievement_path(@achievement)
+    else
+      render :edit
+    end
+  end
+
   def show
     @achievement = Achievement.find(params[:id])
+  end
+
+  def destroy
+    Achievement.destroy(params[:id])
+    redirect_to achievements_path
   end
 
   private
